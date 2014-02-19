27b846e99eaef6b1de413e7c5b05815ac2f58623
Andy <agup2001@msn.com>
2014-02-19 16:23:47 -0700

    fixes #1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var retrieve = require("./src/retrieve");
+var retrieve = require("./src/retriever");
 var app = express();
 
 var childProcess = require("child_process"),
